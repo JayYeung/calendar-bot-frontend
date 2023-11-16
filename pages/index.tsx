b84d02096b946e92032f3bfa9386d425ebbd42a3
JayYeung <JayYeungScout@gmail.com>
2023-11-15 19:39:21 -0800

    added images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,93 +1,103 @@
 import Head from 'next/head';
 import { useState } from 'react';
 
-export default function OceanEquations() {
-  const [showDensitySolution, setShowDensitySolution] = useState(false);
-  const [showDepthSolution, setShowDepthSolution] = useState(false);
-  const [showShadowSolution, setShowShadowSolution] = useState(false);
+export default function CarbonBiologicalPump() {
+  const [showIntro, setShowIntro] = useState(false);
+  const [showPhotosynthesis, setShowPhotosynthesis] = useState(false);
+  const [showJourney, setShowJourney] = useState(false);
+  const [showMicrobial, setShowMicrobial] = useState(false);
+  const [showImpact, setShowImpact] = useState(false);
+
   return (
     <>
       <Head>
-        <title>Ocean Equations</title>
-        <meta name="description" content="Learn about key ocean equations" />
+        <title>Carbon Biological Pump</title>
+        <meta name="description" content="In-depth Exploration of the Carbon Biological Pump and Its Impact on Earth's Climate" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
-      <div className="text-center bg-cover min-h-screen" style={{ backgroundImage: "url('/joseph-barrientos-oQl0eVYd_n8-unsplash.jpg')" }}>
-        
-        <header className="py-4 bg-opacity-60 bg-blue-500">
-          <h1 className="text-4xl font-bold">Ocean Equations</h1>
-          <p className="italic">By <a href="https://jayyeung.vercel.app">Jay Yeung</a></p>
-          <nav className="space-x-4">
-            <a href="#density" className="text-lg">Density</a>
-            <a href="#depth" className="text-lg">Depth</a>
-            <a href="#shadow" className="text-lg">Shadow</a>
-          </nav>
+      <div className="bg-cover min-h-screen" style={{ backgroundImage: "url('/ocean-deep.jpg')" }}>
+
+        <header className="py-6 bg-opacity-80 bg-indigo-700 text-white">
+          <div className="container mx-auto">
+            <h1 className="text-5xl font-bold">Exploring the Carbon Biological Pump</h1>
+            <p className="italic text-lg mt-2">By Joshua Xiong</p>
+          </div>
         </header>
-        
-        <section id="density" className="py-4 px-20 text-left bg-opacity-60 bg-white">
-          <h2 className="text-2xl font-semibold">Density</h2>
-          <p>Equation: Density (D) = M/V</p>
-          <p>M = Mass of the object
-          <br></br>V = Volume occupied by the object</p>
-          <p>Problem: Calculate the density of a unicorn inflatable filled with air that takes up 0.1 m^3 and weighs 3 lbs.</p>
-          <button 
-            onClick={() => setShowDensitySolution(!showDensitySolution)}
-            className="mt-2 px-2 py-0.5 bg-cyan-500 text-white rounded hover:bg-blue-600 transition duration-300"
-          >
-            Show/Hide Solution
-          </button>
-          {showDensitySolution && (
-            <p>
-              Density (D) = 3 lbs/0.1 m^3
-              <br></br>D = 30 lbs/m^3
-            </p>
+
+        <main className="container mx-auto p-6 bg-white rounded shadow-lg mt-6 mb-6">
+          <section>
+            <h2 className="text-3xl font-semibold">Introduction to the Carbon Biological Pump</h2>
+            {showIntro ? (
+              <>
+            <p>The biological carbon pump is an essential yet often overlooked component of the global carbon cycle. It plays a pivotal role in regulating Earth's climate by managing atmospheric carbon dioxide levels through a series of complex biological and chemical processes...</p>
+                <img src="/carbon-cycle.png" alt="Carbon Cycle" className="mt-4 w-full rounded" />
+              </>
+            ) : (
+              <button onClick={() => setShowIntro(true)} className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800 transition duration-300">Learn More</button>
+            )}
+          </section>
+
+          {showIntro && (
+            <section className="mt-6">
+              <h2 className="text-3xl font-semibold">The Role of Photosynthesis and Phytoplankton</h2>
+              {showPhotosynthesis ? (
+                <>
+            <p>Phytoplankton, microscopic organisms in the ocean, are the starting point of this incredible process. Through photosynthesis, they absorb carbon dioxide and sunlight to produce oxygen and sugars, setting the stage for carbon sequestration...</p>
+                  <img src="/phytoplankton.jpeg" alt="Phytoplankton" className="mt-4 w-full rounded" />
+                </>
+              ) : (
+                <button onClick={() => setShowPhotosynthesis(true)} className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800 transition duration-300">Discover More</button>
+              )}
+            </section>
           )}
-        </section>
-        
-        <section id="depth" className="py-4 px-20 text-left bg-opacity-60 bg-white">
-          <h2 className="text-2xl font-semibold">Depth</h2>
-          <p>Equation: Depth (D) = V * T / 2</p>
-          <p>V = Speed of sound in the medium (e.g., seawater)
-          <br></br>T = Time taken for the soundwave to travel and return</p>
-          <p>Problem: If a soundwave takes 0.86 seconds to travel from the hull of a ship to the bottom of the ocean and back, how deep is the water? (hint- The average speed of sound in seawater is 1533 m/s.)</p>
-          <button 
-            onClick={() => setShowDepthSolution(!showDepthSolution)}
-            className="mt-2 px-2 py-0.5 bg-cyan-500 text-white rounded hover:bg-blue-600 transition duration-300"
-          >
-            Show/Hide Solution
-          </button>
-          {showDepthSolution && (
-            <p>
-              Depth (D) = 1533 m/s * 0.86 s / 2
-              <br></br>D = 659.19 m
-            </p>
+
+          {showPhotosynthesis && (
+            <section className="mt-6">
+              <h2 className="text-3xl font-semibold">Journey of Organic Matter: From Surface to Depths</h2>
+              {showJourney ? (
+                <>
+            <p>A significant portion of the organic matter produced by phytoplankton, along with dead organisms, forms what is known as 'marine snow.' This material slowly descends to the ocean floor, carrying trapped atmospheric carbon deep into the ocean...</p>
+                  <img src="/marine-snow.jpeg" alt="Marine Snow" className="mt-4 w-full rounded" />
+                </>
+              ) : (
+                <button onClick={() => setShowJourney(true)} className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800 transition duration-300">Explore Further</button>
+              )}
+            </section>
           )}
-        </section>
-        
-        <section id="shadow" className="py-4 px-20 text-left bg-opacity-60 bg-white">
-          <h2 className="text-2xl font-semibold">Shadow</h2>
-          <p>Equation: A / 360 degrees = K / C</p>
-          <p>A = Angle covered by the shadow
-          <br></br>K = Known distance corresponding to the shadow angle
-          <br></br>C = Total circumference or distance to be determined</p>
-          <p>Problem: If a shadow on Earth covers 7.2 degrees, and the known distance is 800 km, what is the total circumference of Earth?</p>
-          <button 
-            onClick={() => setShowShadowSolution(!showShadowSolution)}
-            className="mt-2 px-2 py-0.5 bg-cyan-500 text-white rounded hover:bg-blue-600 transition duration-300"
-          >
-            Show/Hide Solution
-          </button>
-          {showShadowSolution && (
-            <p>
-              7.2 degrees / 360 degrees = 800 km / C
-              <br></br>C ≈ 40000 km
-            </p>
+
+          {showJourney && (
+            <section className="mt-6">
+              <h2 className="text-3xl font-semibold">Microbial Activity and Deep-Sea Carbon Storage</h2>
+              {showMicrobial ? (
+                <>
+            <p>In the deep ocean, microbes break down this sunken organic matter. This process releases carbon dioxide back into the ocean, where it can be stored in deep-sea sediments for centuries, contributing to the long-term storage of carbon...</p>
+                  <img src="/deep-sea-microbes.jpeg" alt="Deep Sea Microbes" className="mt-4 w-full rounded" />
+                </>
+              ) : (
+                <button onClick={() => setShowMicrobial(true)} className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800 transition duration-300">Learn More</button>
+              )}
+            </section>
           )}
-        </section>
-        
-        <footer className="w-full py-4 text-center text-gray-500 text-sm bg-opacity-60 bg-blue-500">
-          &copy; 2023 . All rights reserved. By <a href="https://jayyeung.vercel.app">Jay Yeung</a>
+
+          {showMicrobial && (
+            <section className="mt-6">
+              <h2 className="text-3xl font-semibold">Impact of Eutrophication and Climate Change</h2>
+              {showImpact ? (
+                <>
+            <p>Eutrophication and climate change pose significant threats to the efficiency of the carbon biological pump. These factors can disrupt the delicate balance of ocean ecosystems, affecting the pump's ability to sequester carbon effectively...</p>
+                  <img src="/eutrophication.jpeg" alt="Eutrophication" className="mt-4 w-full rounded" />
+                </>
+              ) : (
+                <button onClick={() => setShowImpact(true)} className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800 transition duration-300">Discover More</button>
+              )}
+            </section>
+          )}
+        </main>
+
+        <footer className="py-4 text-center text-gray-600 bg-opacity-80 bg-indigo-700">
+          &copy; 2023 . All rights reserved. By Joshua Xiong
         </footer>
       </div>
     </>
